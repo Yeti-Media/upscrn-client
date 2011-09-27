e276b6a8a42fbe8c0c502c1426aab4dcd017377a
Nicolas Santa <nicolas55ar@gmail.com>
2011-09-27 19:59:08 -0300

    add support for new API

--- lib/upscrn_client/client.rb
+++ lib/upscrn_client/client.rb
@@ -7,22 +7,14 @@ module UpscrnClient
          yield self
        end
 
-    end
 
-    attr_accessor :auth_token
+      #### WARNING: THE CLASS METHODS ARE DEPRECATED
 
-
-
-
-
-    def initialize(auth_token = nil)
-      @auth_token = auth_token || self.class.auth_token
-    end
-
-      # Upload a screenshot to the upscrn server.
+       # Upload a screenshot to the upscrn server.
       # Pass :project_id in the options to upload to a particular project
 
       def upload_screenshot(filename, auth_token, options = {})
+        puts "DEPRECATED: will be removed on version 0.3"
         #puts "filepath: #{filepath}"
         @result = Hash.new
         @result['success'] = true
@@ -52,8 +44,11 @@ module UpscrnClient
       # return a list of projects for a given user
       # list is returned in json format
       def projects(auth_token)
+        puts "DEPRECATED: will be removed on version 0.3"
         perform('get', 'projects', auth_token)
       end
+
+      private
 
       def perform(verb,action,auth_token, params={})
         action = [action, 'json'].join('.')
@@ -61,7 +56,116 @@ module UpscrnClient
         url = url + "?auth_token=#{auth_token}"
         JSON.parse(RestClient.send(verb,url,params).body)
       end
+
     end
+
+    attr_accessor :auth_token , :resources , :params
+
+
+
+    def initialize(auth_token = nil)
+      @auth_token = auth_token || self.class.auth_token
+      @resources = []
+      @params = {}
+    end
+
+
+    def projects(params = {})
+       tap do
+         @resources = ['projects']
+         @params.merge!(params)
+       end
+    end
+
+    def screenshots(params = {})
+       tap do
+         @resources += ['screenshots']
+         @params.merge!(params)
+       end
+    end
+
+    def comments(params = {})
+       tap do
+         @resources += ['comments']
+         @params.merge!(params)
+       end
+    end
+
+    def videos(params = {})
+       tap do
+         @resources += ['videos']
+         @params.merge!(params)
+       end
+    end
+
+    def project(id , params = {})
+      tap do
+        @resources = ['projects',id]
+        @params.merge!(params)
+      end
+    end
+
+    def screenshot(id , params = {})
+      tap do
+        @resources += ['screenshots',id]
+        @params.merge!(params)
+      end
+    end
+
+    def video(id , params = {})
+      tap do
+        @resources += ['videos',id]
+        @params.merge!(params)
+      end
+    end
+
+    def comment(params = {})
+      tap do
+        @resources += ['comments']
+        @params.merge!(params)
+      end
+    end
+
+
+
+    def get(clean = true)
+      result = client[build_path(@auth_token,@resources,@params)].get
+      self.clean if clean
+      JSON.parse result
+    end
+
+    def post(clean = true)
+      result = client[build_path(@auth_token,@resources)].post(@params)
+      self.clean if clean
+      JSON.parse result
+    end
+
+    def put(clean = true)
+      result = client[build_path(@auth_token,@resources)].put(@params)
+      self.clean if clean
+      JSON.parse result
+    end
+
+    def clean
+      tap do
+        @resources = []
+        @params = {}
+      end
+    end
+
+    private
+
+    def client
+      @client ||= RestClient::Resource.new('http://upscrn.com')
+    end
+
+    def build_path(auth_token,resources , params = {})
+      url = resources.join('/')
+      url += '.json'
+      url += "?" + params.mer(:auth_token => auth_token).to_a.map{|p| p.join('=')}.join('&')
+      URI.encode(request)
+    end
+
   end
 end
 
